b60c4e6d1df99682b2b8fade9313e61908d185bd
SimonHung8 <simon850226@gmail.com>
2023-01-04 23:50:06 +0800

    refactor: modify meet service

--- services/meet-service.js
+++ services/meet-service.js
@@ -6,7 +6,7 @@ const meetService = {
       const HouseId = parseInt(req.body.HouseId)
       const ConditionId = parseInt(req.body.ConditionId)
       const UserId = req.user.id
-      if (!HouseId || !ConditionId) return cb(null, 400, { message: 'HouseId and ConditionId are required!' })
+      if (!HouseId || !ConditionId) return cb(null, 400, { message: '物件或自定義條件不存在' })
 
       // 反查house與condition
       const house = await House.findOne({
@@ -27,8 +27,7 @@ const meetService = {
       const [meet, created] = await Meet.findOrCreate({
         where: {
           HouseId,
-          ConditionId,
-          UserId
+          ConditionId
         }
       })
       if (!created) return cb(null, 400, { message: '此條件已符合' })
@@ -40,12 +39,12 @@ const meetService = {
   deleteMeet: async (req, cb) => {
     try {
       const id = parseInt(req.params.id)
-      console.log(id)
       const UserId = req.user.id
-      const isMet = await Meet.findOne({
-        where: { id, UserId }
-      })
+      const isMet = await Meet.findByPk(id)
       if (!isMet) return cb(null, 400, { message: '條件原本就不符合' })
+      // 檢查是否為使用者的物件
+      const house = await House.findOne({ where: { id: isMet.HouseId, UserId } })
+      if (!house) return cb(null, 400, { message: '條件原本就不符合' })
       const meet = await isMet.destroy()
       return cb(null, 200, { meet: meet.toJSON() })
     } catch (err) {
