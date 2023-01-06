846525d12640efbd74ce8b61e32c6d7792d4f292
SimonHung8 <simon850226@gmail.com>
2023-01-06 18:13:06 +0800

    feat: add lineAuth feature

--- controllers/line-auth-controller.js
+++ controllers/line-auth-controller.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const lineAuthService = require('../services/line-auth-service')
 const responseHelper = require('../helpers/response-helper')
 
@@ -5,8 +6,16 @@ const lineAuthController = {
   getLink: (req, res, next) => {
     lineAuthService.getLink(req, (err, code, data) => responseHelper(res, next, err, code, data))
   },
-  getToken: (req, res, next) => {
-    lineAuthService.getToken(req, (err, code, data) => responseHelper(res, next, err, code, data))
+  getToken: (req, res) => {
+    lineAuthService.getToken(req, (err) => {
+      if (err) {
+        if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError' || err.name === 'NotBeforeError' || err.message === '使用者資訊錯誤') {
+          return res.send('<h3>使用者資訊錯誤</h3>')
+        }
+        return res.send('<h3>Server Error</h3>')
+      }
+      return res.sendFile(path.join(__dirname, '../views', 'success.html'))
+    })
   }
 }
 
