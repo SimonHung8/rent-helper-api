b9d43cb9b90a8e6d08785ea5e4024c0223cf7187
SimonHung8 <simon850226@gmail.com>
2023-01-09 21:36:05 +0800

    fix: update cron helper

--- helpers/cron-helper.js
+++ helpers/cron-helper.js
@@ -8,14 +8,10 @@ module.exports = async () => {
   cron.schedule(process.env.FREQUENCY, async () => {
     try {
       const searches = await Search.findAll()
-      console.log(searches.length)
       if (!searches.length) return
       for (const search of searches) {
-        console.log(search.name)
         // 設定header
-        console.log('setting header')
         const headers = await scrapeHelper.setListHeader()
-        console.log('setting header done')
         headers.cookie += `;urlJumpIp=${search.region};`
         if (search.region === 1) {
           headers.cookie += `urlJumpIpByTxt=${encodeURI('台北市')};`
@@ -25,9 +21,7 @@ module.exports = async () => {
         }
         // 設定要請求的網址
         const targetURL = scrapeHelper.setTargetURL(search)
-        console.log('fetching')
         const targetRes = await fetch(targetURL, { headers })
-        console.log('fetch done')
         // 請求失敗
         if (targetRes.status !== 200) throw new Error('爬不到資料，快來檢查一下')
 
@@ -36,22 +30,18 @@ module.exports = async () => {
         const originalResults = search.results.split(',').map(item => Number(item))
         const newResults = targetData.data.data.map(item => item.post_id)
         const differentResults = newResults.filter(item => !originalResults.includes(item))
-        console.log('differentResults')
-        console.log(differentResults)
-        if (!differentResults.length) return
-        // 更新資料庫
-        console.log('updating database')
-        await search.update({ results: newResults.join(',') })
-        console.log('update done')
-        // 發送line通知
-        const user = await User.findByPk(search.UserId, {
-          attributes: ['token'],
-          raw: true
-        })
-        if (!user) return
-        console.log('sending line')
-        await notifyHelper(search.name, differentResults, user.token)
-        console.log('see line')
+        if (differentResults.length) {
+          // 更新資料庫
+          await search.update({ results: newResults.join(',') })
+          // 發送line通知
+          const user = await User.findByPk(search.UserId, {
+            attributes: ['token'],
+            raw: true
+          })
+          if (user) {
+            await notifyHelper(search.name, differentResults, user.token)
+          }
+        }
       }
     } catch (err) {
       console.error(err)
