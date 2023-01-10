c3fbb97e45af8dd02842969678bc36d5c250353a
SimonHung8 <simon850226@gmail.com>
2023-01-10 19:22:51 +0800

    add fail page

--- routes/modules/line-auth.js
+++ routes/modules/line-auth.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
+const path = require('path')
 const lineAuthController = require('../../controllers/line-auth-controller')
+
+// 使用者取消驗證
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../views', 'fail.html')))
 
 // 取得驗證
 router.post('/', lineAuthController.getToken)
