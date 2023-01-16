238b24d479067be8714c9fb2628adb2702253a98
SimonHung8 <simon850226@gmail.com>
2023-01-16 11:23:17 +0800

    refactor: modify get line token

--- services/line-auth-service.js
+++ services/line-auth-service.js
@@ -9,8 +9,12 @@ const lineAuthService = {
       const data = jwt.verify(state, process.env.LINE_AUTH_SECRET)
       const user = await User.findByPk(data.id)
       if (!user) throw new Error('使用者資訊錯誤')
-      const tokenResp = await fetch(`https://notify-bot.line.me/oauth/token?grant_type=authorization_code&code=${code}&client_id=${process.env.LINE_CLIENT_ID}&client_secret=${process.env.LINE_CLIENT_SECRET}&redirect_uri=${process.env.LINE_REDIRECT_URI}`, {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      // 取得notify token
+      const tokenResp = await fetch('https://notify-bot.line.me/oauth/token', {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=authorization_code&code=${code}&client_id=${process.env.LINE_CLIENT_ID}&client_secret=${process.env.LINE_CLIENT_SECRET}&redirect_uri=${process.env.LINE_REDIRECT_URI}`,
         method: 'POST'
       })
       const tokenData = await tokenResp.json()
